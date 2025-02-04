3490d5a132af0662bc2c9ea076e16655b6f942f4
minhtam <minhtamnhgp03@gmail.com>
2025-02-04 22:25:15 +0700

    feat: ui new icon for teacher sidebar

---
+++ client/src/components/icons/ReviewIdeaIcon.tsx
@@ -0,0 +1,20 @@
+
+import React from 'react'
+
+const ReviewIdeaIcon = () => {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'>
+      <path
+        fill='none'
+        stroke='currentColor'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='1.5'
+        d='M9 5H7a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-2M9 5a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2M9 5h6m-3 7l2 2m0 0l2 2m-2-2l-2 2m2-2l-2-2'
+        color='currentColor'
+      />
+    </svg>
+  )
+}
+
+export default ReviewIdeaIcon
