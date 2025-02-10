c9fc7dbc9f7755884ff84d9878e7e4155dd0f359
vietqtran <tranquocviet1303@gmail.com>
2025-02-10 20:30:46 +0700

    fix empty project details

--- client/src/app/(root)/project/_components/ProjectPage.tsx
+++ client/src/app/(root)/project/_components/ProjectPage.tsx
@@ -1,12 +1,21 @@
 'use client'
 
+import Image from 'next/image'
 import ProjectDetails from './ProjectDetails'
 import React from 'react'
 import { useProject } from '@/hooks'
 
 const ProjectPage = () => {
   const { project } = useProject()
-  return <ProjectDetails project={project} />
+  if (project) {
+    return <ProjectDetails project={project} />
+  }
+  return (
+    <div className='w-full flex items-center justify-center flex-col'>
+      <Image width={500} height={500} src='/images/empty-box.png' alt='loading' />
+      <p>You don&apos;t have any projects yet. Search for members and create together!</p>
+    </div>
+  )
 }
 
 export default ProjectPage
--- client/src/hooks/useProject.ts
+++ client/src/hooks/useProject.ts
@@ -3,7 +3,6 @@ import { useEffect, useState } from 'react'
 import { Project } from '@/types/project.type'
 import { RootState } from '@/types/store.type'
 import instance from '@/utils/axios'
-import { toast } from 'sonner'
 import { useAppSelector } from './useStore'
 
 export const useProject = () => {
@@ -16,9 +15,6 @@ export const useProject = () => {
   const getProject = async () => {
     if (!user?._id) return
     const { data } = await instance.get(`/project/?userIds=${user?._id}`, { withCredentials: true })
-    if (!data.data) {
-      toast.error('Error occur at getting project details!')
-    }
     setProject(data.data)
   }
 
