8a4fc4c88706bf5baa20ae1736d11b1f27d29a3b
giakhanh102003 <ngokienhuy550@gmail.com>
2025-02-28 00:22:17 +0700

    chore(component): delete axios in component CreateIdeaStudent

--- client/src/app/(root)/create-idea/_components/student/CreateIdeaStudent.tsx
+++ client/src/app/(root)/create-idea/_components/student/CreateIdeaStudent.tsx
@@ -1,6 +1,5 @@
 'use client'
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 import { useAppSelector } from '@/hooks'
 import { Field } from '@/types/field.type'
 import { Major } from '@/types/major.type'
