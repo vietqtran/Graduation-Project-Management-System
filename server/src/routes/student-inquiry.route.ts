d26099c14a2c880f34c8c7b14b299fdc45ad94ef
nlqtrang <nguyenqtrang1409@gmail.com>
2025-03-20 01:48:33 +0700

    feat: add student inquiry routes and controller

---
+++ server/src/routes/student-inquiry.route.ts
@@ -0,0 +1,15 @@
+import { StudentInquiryController } from "@/controllers/student-inquiry.controller";
+import { StaffAnswerStudentInquiryDto, StaffGetListStudentInquiriesDto } from "@/dtos/student-inquiry/manage-student-inquiry.dto";
+import { StudentCreateInquiryDto, StudentGetListInquiriesDto } from "@/dtos/student-inquiry/student-inquiry.dto";
+import { validateDto } from "@/middlewares/validate.middleware";
+import { Router } from "express";
+
+const router = Router()
+const studentInquiryController = new StudentInquiryController()
+
+router.post('/student-create-inquiry', validateDto(StudentCreateInquiryDto), studentInquiryController.studentCreateInquiry)
+router.post('/student-get-list-inquiries', validateDto(StudentGetListInquiriesDto), studentInquiryController.studentGetListInquiries)
+router.post('/staff-answer-student-inquiry', validateDto(StaffAnswerStudentInquiryDto), studentInquiryController.staffAnswerStudentInquiry)
+router.post('/staff-get-list-student-inquiries', validateDto(StaffGetListStudentInquiriesDto), studentInquiryController.staffGetListStudentInquiries)
+
+export { router as studentInquiryRoutes }
